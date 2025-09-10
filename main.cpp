fed233fbc94ff39ec119e2dabc0e7b4f0baafac6
sorenahelali <sorenahelali13911@gmail.com>
2025-09-10 21:32:11 +0330

    Sweetness or not sweetness detector without image
    Sweetness or not sweetness detector without image

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+void set1 ()
+{
+    puts("| 1 | 2 | 3 |");
+    puts("| 4 | 5 | 6 |");
+    puts("| 7 | 8 | 9 |");
+}
+void set2()
+{
+    puts("+ or * or / or - or");
+}
+
+int main()
+{
+    set1();
+    puts("please enter number ");
+    int num;
+    string name[num];
+    cin>> num;
+    cin >> name[num];
+
+
+
+}
